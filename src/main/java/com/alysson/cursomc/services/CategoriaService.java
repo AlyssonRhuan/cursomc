68cd9cdac421392dda54775c8538b473128ca8f8
AlyssonRhuan <alysson_salgado@hotmail.com>
2018-09-17 18:16:40 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/alysson/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.alysson.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alysson.cursomc.domain.Categoria;
+import com.alysson.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;	
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		return categoria.orElse(null);				
+	}
+}
