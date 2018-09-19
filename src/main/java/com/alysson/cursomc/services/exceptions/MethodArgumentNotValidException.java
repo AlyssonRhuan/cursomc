7f44d2a89e3546d98f2b8d269f99192f0944cb09
AlyssonRhuan <alysson_salgado@hotmail.com>
2018-09-19 16:26:34 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/alysson/cursomc/services/exceptions/MethodArgumentNotValidException.java
@@ -0,0 +1,13 @@
+package com.alysson.cursomc.services.exceptions;
+
+public class MethodArgumentNotValidException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public MethodArgumentNotValidException(String message) {
+		super(message);
+	}
+
+	public MethodArgumentNotValidException(String message, Throwable cause) {
+		super(message, cause);
+	}	
+}
