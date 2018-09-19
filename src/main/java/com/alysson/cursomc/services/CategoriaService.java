f5adcd971721d9625f4e712c660c8673905d1d9a
AlyssonRhuan <alysson_salgado@hotmail.com>
2018-09-19 14:04:43 -0300

    Deletando uma Categoria

--- src/main/java/com/alysson/cursomc/services/CategoriaService.java
+++ src/main/java/com/alysson/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.alysson.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.alysson.cursomc.domain.Categoria;
 import com.alysson.cursomc.repositories.CategoriaRepository;
+import com.alysson.cursomc.services.exceptions.DataIntegrityException;
 import com.alysson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,17 @@ public class CategoriaService {
 	public Categoria update(Categoria cat) {
 		findById(cat.getId());
 		return categoriaRepository.save(cat);
+	}
+	
+	public void delete(Integer id) {
+		findById(id);
+		
+		try {
+			categoriaRepository.deleteById(id);	
+		} 
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos.");
+		}		
+		
 	}
 }
