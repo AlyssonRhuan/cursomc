96068e719afb6b73550eb3a82ab478fb6e3e30fe
AlyssonRhuan <alysson_salgado@hotmail.com>
2018-09-24 10:14:45 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/alysson/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/alysson/cursomc/services/AbstractEmailService.java
@@ -21,8 +21,8 @@ public abstract class AbstractEmailService implements EmailService{
 	private String sender;
 	
 	@Autowired
-	private TemplateEngine templateEngine; 
-	
+	private TemplateEngine templateEngine; 	
+
 	@Autowired
 	private JavaMailSender javaMailSender; 
 
@@ -32,41 +32,13 @@ public abstract class AbstractEmailService implements EmailService{
 		sendEmail(smm);
 	} 
 
-	@Override		
-	public void sendOrderConfirmationHtmlEmail(Pedido pedido) {
-		try {
-			MimeMessage mm = prepareMimeMessageFromPedido(pedido);
-			sendHtmlEmail(mm);	
-		} catch (MessagingException e) {
-			sendOrderConfirmationEmail(pedido);
-		}	
-	}
-	
-	private MimeMessage prepareMimeMessageFromPedido(Pedido pedido) throws MessagingException {
-		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-		
-		helper.setTo(pedido.getCliente().getEmail());
-		helper.setFrom(sender);
-		helper.setSubject("Pedido confirmado!");
-		helper.setSentDate(new Date(System.currentTimeMillis()));
-		helper.setText(htmlFromTemplatePedido(pedido), true);
-		
-		return mimeMessage;
-	}
-
-	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido pedido) {
-		
-		SimpleMailMessage smm = new SimpleMailMessage();
-		
+	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido pedido) {		
+		SimpleMailMessage smm = new SimpleMailMessage();		
 		smm.setTo(pedido.getCliente().getEmail());
-		smm.setFrom(sender);
-		
+		smm.setFrom(sender);		
 		smm.setSubject("Pedido confirmado!");
-		smm.setSentDate(new Date(System.currentTimeMillis()));
-		
-		smm.setText(pedido.toString());
-		
+		smm.setSentDate(new Date(System.currentTimeMillis()));		
+		smm.setText(pedido.toString());		
 		return smm;
 	}
 	
@@ -75,4 +47,26 @@ public abstract class AbstractEmailService implements EmailService{
 		context.setVariable("pedido", pedido);
 		return templateEngine.process("email/confirmacaoPedido", context);
 	}	
+
+	@Override		
+	public void sendOrderConfirmationHtmlEmail(Pedido pedido) {
+		try {
+			MimeMessage mm = prepareMimeMessageFromPedido(pedido);
+			sendHtmlEmail(mm);	
+		} 
+		catch (MessagingException e) {
+			sendOrderConfirmationEmail(pedido);
+		}	
+	}
+	
+	private MimeMessage prepareMimeMessageFromPedido(Pedido pedido) throws MessagingException {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);		
+		helper.setTo(pedido.getCliente().getEmail());
+		helper.setFrom(sender);
+		helper.setSubject("Pedido confirmado!");
+		helper.setSentDate(new Date(System.currentTimeMillis()));
+		helper.setText(htmlFromTemplatePedido(pedido), true);		
+		return mimeMessage;
+	}
 }
