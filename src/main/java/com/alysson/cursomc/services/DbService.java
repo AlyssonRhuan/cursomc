b433e8f0861962b60e31acccd554a5cc125612c9
AlyssonRhuan <alysson_salgado@hotmail.com>
2018-09-24 11:08:12 -0300

    Salvando perfis de usuarios na base de dados

--- src/main/java/com/alysson/cursomc/services/DbService.java
+++ src/main/java/com/alysson/cursomc/services/DbService.java
@@ -20,6 +20,7 @@ import com.alysson.cursomc.domain.PagamentoComCartao;
 import com.alysson.cursomc.domain.Pedido;
 import com.alysson.cursomc.domain.Produto;
 import com.alysson.cursomc.domain.enums.EstadoPagamento;
+import com.alysson.cursomc.domain.enums.Perfil;
 import com.alysson.cursomc.domain.enums.TipoCliente;
 import com.alysson.cursomc.repositories.CategoriaRepository;
 import com.alysson.cursomc.repositories.CidadeRepository;
@@ -92,10 +93,13 @@ public class DbService {
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "alysson.salgado@gauge.com.br", "12345678900", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("12345"));
+		Cliente cli1 = new Cliente(null, "Maria Silva", "alysson.salgado@gauge.com.br", "58150987606", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("12345"));
+		Cliente cli2 = new Cliente(null, "Alysson Rhuan", "alyssonr.1993@gmail.com", "91499038615", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("12345"));
+		cli2.setPerfil(Perfil.ADMIN);
 
 		Endereco e1 = new Endereco(null, "Rua Floresta", "300", "Apartamento 203", "Jardim", "38220834", cli1, c1);	
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);	
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2105", null, "Centro", "12123123", cli2, c2);	
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
@@ -120,10 +124,12 @@ public class DbService {
 		p3.getItens().addAll(Arrays.asList(ip2));
 		
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-		
+
 		cli1.getTelefones().addAll(Arrays.asList("36231785", "99998888"));
+		cli2.getTelefones().addAll(Arrays.asList("36231785", "99998888"));
 		
-		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));		
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
@@ -152,8 +158,8 @@ public class DbService {
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
